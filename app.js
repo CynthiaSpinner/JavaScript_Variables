7828f50320219f3f0181d271abdfc104e6562c03
Cynthia <CynthiaSpinner@gmail.com>
2024-04-28 10:37:31 -0500

    finished javascript variables

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 console.log("Hello World!\n==========\n");
 console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+  "why is this console logged? ---> Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = `Cynthia`;
+let lastName = `Spinner`;
+var age = 42;
 
-// Exercise 2
+let language = `Javascript`;
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`My first name is: ${firstName}`);
+console.log(`My last name is: ${lastName}`);
+console.log(`My age is: ${age}`);
+console.log();
+console.log(`This was written in: ${language}`);
